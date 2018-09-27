277125c2113b3c35b21eea4402fda48e63e5ee3a
Artemius Visotski <artemius.visotski@gmail.com>
2018-09-28 00:20:36 +0300

    quadratic-equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  
+ equation = equation.split(" ")
+  var a = Number(equation[0])
+  var b = Number(equation[3] === "-" ? - equation[4] : equation[4])
+  var c = Number(equation[7] === "-" ? -equation[8] : equation[8])
+
+  var dicr = b * b - 4 * a * c;
+  var x2 = ((b * (-1)) - Math.sqrt(dicr)) / (2 * a);
+  var x1 = ((b * (-1)) + Math.sqrt(dicr)) / (2 * a);
+  var arr = [];
+  x1 = Math.round(x1);
+  x2 = Math.round(x2);
+  arr.push(x1);
+  arr.push(x2);
+  if (arr[0] > arr[1]) {
+    var temp = arr[0];
+    arr[0] = arr[1];
+    arr[1] = temp;
+  }
+  return arr;
 }
